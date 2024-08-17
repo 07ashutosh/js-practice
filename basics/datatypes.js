44bff5b8d1c903c3eeacb253559cbb5cc1311425
07ashutosh <160994891+07ashutosh@users.noreply.github.com>
2024-08-17 14:23:55 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+// string data type used in "" or ''
+let name ="ashutosh"
+
+// number datatype 
+let age = 21
+
+
+// boolean trueor false
+let logic ="true"
+
+// null is a stand alone value or null is a object accourding to typeof fun
+
+// undefined
+let city
+
+// symbol for uniqueness
+
+// object
+
+// typeof
+console.log(typeof "ashutosh")
