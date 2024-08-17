14f43cbf4fb39b93b3c2a347ea9ca10c728aa19f
07ashutosh <160994891+07ashutosh@users.noreply.github.com>
2024-08-17 13:58:41 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountid = 6969
+let accountemail ="ashutosh@gmail.com"
+var password ="12345"
+
+accountcity="jaypur"
+
+accountemail="abc@gmail.com"
+password="7410"
+accountcity="up"
+
+console.table([accountid,accountemail,password,accountcity])
