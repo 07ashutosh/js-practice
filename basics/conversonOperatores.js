f5546f1ed5f16d5ac5de14d132c617eecbb735ba
07ashutosh <160994891+07ashutosh@users.noreply.github.com>
2024-08-17 15:00:32 +0000

    conversonOperatores

---
+++ basics/conversonOperatores.js
@@ -0,0 +1,43 @@
+let score = undefined
+
+console.log(typeof score);
+
+let conertedScore = Number(score)
+//console.log(typeof conertedScore);
+//console.log(conertedScore);// if score 33abc hai too wo convert nahi hoga or NaN(not a number) batayega
+
+// boolean value pr 1 ya 0 dega according to value (true or false)
+
+// null hoga to 0(zero) dega
+
+// undefined ho to NaN dega
+
+// number to boolean converson
+
+let loggic = 1
+
+let convertedlog = Boolean(loggic)
+//console.log(convertedlog);
+
+/*
+1=>true , 0=>false
+""=> false
+"ashutosh"=>true
+*/
+
+
+// string converson
+
+let someno = 7
+let convertedno = String(someno)
+console.log(typeof convertedno);
+console.log(convertedno);
+
+
+
+
+
+
+
+
+
