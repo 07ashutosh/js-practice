bfb43e447869532f6d27a3e0a8e1c25392ef3982
07ashutosh <160994891+07ashutosh@users.noreply.github.com>
2024-08-18 13:44:53 +0000

    array

---
+++ basic2/array1.js
@@ -0,0 +1,16 @@
+const arr =[1,2,3,4,5]
+const myarr2  =  new Array("ashu","vin","vik","babu")
+// console.log(myarr2[0])
+// myarr2.push(8)
+// console.log(myarr2);
+
+// myarr2.pop()
+// console.log(myarr2);
+// myarr2.unshift(9)
+// myarr2.shift()
+// console.log(myarr2);
+
+ 
+
+
+
