020216d1c5e3e692e7af5ae3eddd88accc1aff13
07ashutosh <160994891+07ashutosh@users.noreply.github.com>
2024-08-18 08:46:43 +0000

    operatons

--- basics/conversonOperatores.js
+++ basics/conversonOperatores.js
@@ -1,6 +1,6 @@
 let score = undefined
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let conertedScore = Number(score)
 //console.log(typeof conertedScore);
@@ -30,8 +30,57 @@ let convertedlog = Boolean(loggic)
 
 let someno = 7
 let convertedno = String(someno)
-console.log(typeof convertedno);
-console.log(convertedno);
+// console.log(typeof convertedno);
+// console.log(convertedno);
+
+
+// *********** Operations**********************
+// neative value
+
+let vlaue = 3
+let nagval = -vlaue
+// console.log(nagval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/4);
+// console.log(2*9);
+// console.log(2**3);
+// console.log(2%3);
+
+// ***** string to number converson*****
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"3");
+// console.log("1"+2+3);
+// console.log(1+"2"+3);
+
+
+// console.log((2+3)*(5/6)); // use () for make your code readable
+// console.log(+true);
+// console.log(+"");
+
+
+// ***** prefix and postfix operatores
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
 
 
 
